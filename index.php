93ec335484c18060946b16dd11d498a6d1e8cb42
Prabhat <prabhatdash0376@gmail.com>
2021-06-22 08:48:16 +0530

    UPLOAD
    UPLOAD

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>
+        PAGE 1
+    </title>
+</head>
+<body>
+<center>
+    <form method="post" action="page2.php">
+        <input name="age" type="number" placeholder="ENTER AGE">
+        <input type="submit">
+    </form>
+
+</center>
+</body>
+</html>
---
+++ page2.php
@@ -0,0 +1,12 @@
+<?php
+
+$age=$_POST['age'];
+
+if($age >=18){
+    echo "Eligible to vote";
+}
+else{
+    echo "Not Eligible";
+}
+
+?>
